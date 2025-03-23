41186464adeb16bbd93e651788cab8142cd843b0
MAHMUDUL HASAN <mahasan24@students.oamk.fi>
2025-03-23 04:25:09 +0200

    First commit
    First date 21-03-2025

---
+++ server/db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE task (
+    id SERIAL PRIMARY KEY,
+    description TEXT NOT NULL
+);
+
+INSERT INTO task (description) VALUES ('Learn Node.js');
+INSERT INTO task (description) VALUES ('Build a Todo app');
